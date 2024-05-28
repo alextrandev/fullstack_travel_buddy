e8287de658106f9ab0b670527d82ab656a04ebe9
sheilaonyango <edusheila31@gmail.com>
2024-05-28 22:46:24 +0300

    Create hero-banner component

---
+++ frontend/src/Components/Herobanner.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Herobanner = () => {
+  return (
+    <div className="hero-banner">
+      <img
+        src="/Assets/Images/herobannerproject.jpeg"
+        alt="Hero Banner"
+        className="hero-banner-image"
+      />
+    </div>
+  );
+};
+
+export default Herobanner;
