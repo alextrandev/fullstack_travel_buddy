f2684e3436a6e6bd9fc5e83db2f1478e61da30ff
Alex Tran <47502809+alextrandev@users.noreply.github.com>
2024-05-29 14:53:07 +0300

    Hotfix: Fix - resolve leftover problems from FE-002 merge conflict which created two HeroBanner files

--- frontend/src/Components/HeroBanner.jsx
+++ frontend/src/Components/HeroBanner.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import './HeroBanner.css';
-import heroImage from '../assets/hb_blur.jpg';
+import "./Herobanner.css";
+import heroImage from "../assets/hb_blur.jpg";
+
 const HeroBanner = () => {
   return (
-    <div className="hero-banner" style={{ backgroundImage: `url(${heroImage})`  }}>
-      <div className="hero-content">
-       
-        
-      </div>
+    <div
+      className="hero-banner"
+      style={{ backgroundImage: `url(${heroImage})` }}
+    >
+      <div className="hero-content"></div>
     </div>
   );
 };
