e40bf9680c2959354cfafd96b25a1c656698eed0
Alex Tran <47502809+alextrandev@users.noreply.github.com>
2024-05-29 14:44:46 +0300

    Frontend: Style - Revert TestApi component style changes

--- frontend/src/Components/TestApi.jsx
+++ frontend/src/Components/TestApi.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"
 
 export default function TestApi() {
   const [apiData, setApiData] = useState([]);
@@ -7,23 +7,21 @@ export default function TestApi() {
   useEffect(() => {
     axios
       .get(`${import.meta.env.VITE_API_URL}destinations/`)
-      .then((res) => setApiData(res.data))
-      .catch((err) => console.log(err));
+      .then(res => setApiData(res.data))
+      .catch(err => console.log(err))
   }, []);
 
   // console.log(apiData);
 
   return (
     <>
-      {apiData.map((destination) => (
+      {apiData.map(destination =>
         <div key={destination.city}>
-          <img
-            src={destination.imageUrl}
-            alt={`Image of ${destination.city}`}
-          />
+          <img src={destination.imageUrl} alt={`Image of ${destination.city}`} />
           <p>{destination.city}</p>
         </div>
-      ))}
+      )
+      }
     </>
-  );
+  )
 }
