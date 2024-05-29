6d2825e6efcc90b1cb320ff0d967c6b7309a193f
sheilaonyango <edusheila31@gmail.com>
2024-05-29 13:52:37 +0300

    Frontend: map over destination array to render each destination

--- frontend/src/Components/Destination.jsx
+++ frontend/src/Components/Destination.jsx
@@ -2,7 +2,19 @@ const Destination = () => {
   return (
     <div className="destination">
       <h1>Popular Tourist Destinations</h1>
-      <div className="popular-destinations"></div>
+      <div className="popular-destinations">
+        {destinations.map((destination) => (
+          <div className="destination-card" key={destination.city}>
+            <img
+              src={destination.imageUrl}
+              alt={`Image of ${destination.city}`}
+              className="destination-image"
+            />
+            <h3>{destination.city}</h3>
+            <p>{destination.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
