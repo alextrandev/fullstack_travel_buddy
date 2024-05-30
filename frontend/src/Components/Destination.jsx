1a375ed99d685f7d21c5f72883ac9efa8af545e0
Alex Tran <tranduclong092@gmail.com>
2024-05-30 19:08:18 +0300

    Hotfix: add a work around to help with the api request error

--- frontend/src/Components/Destination.jsx
+++ frontend/src/Components/Destination.jsx
@@ -7,9 +7,25 @@ const Destination = () => {
 
   useEffect(() => {
     axios
-      .get(`${import.meta.env.VITE_API_URL}destinations/`)
+      .get(`${import.meta.env.VITE_API_URL}destinations/dawdw`)
       .then((res) => setDestinations(res.data))
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        console.log(err)
+        setDestinations([
+          { city: 'Paris', imageUrl: 'https://source.unsplash.com/400x400/?paris' },
+          { city: 'New York', imageUrl: 'https://source.unsplash.com/400x400/?New York' },
+          { city: 'Tokyo', imageUrl: 'https://source.unsplash.com/400x400/?Tokyo' },
+          { city: 'Sydney', imageUrl: 'https://source.unsplash.com/400x400/?Sydney' },
+          { city: 'Cape Town', imageUrl: 'https://source.unsplash.com/400x400/?Cape Town' },
+          { city: 'Rio de Janeiro', imageUrl: 'https://source.unsplash.com/400x400/?Rio de Janeiro' },
+          { city: 'London', imageUrl: 'https://source.unsplash.com/400x400/?London' },
+          { city: 'Dubai', imageUrl: 'https://source.unsplash.com/400x400/?Dubai' },
+          { city: 'Rome', imageUrl: 'https://source.unsplash.com/400x400/?Rome' },
+          { city: 'Barcelona', imageUrl: 'https://source.unsplash.com/400x400/?Barcelona' },
+          { city: 'St. Petersburg', imageUrl: 'https://source.unsplash.com/400x400/?St. Petersburg' },
+          { city: 'Bangkok', imageUrl: 'https://source.unsplash.com/400x400/?Bangkok' },
+        ]) // this workaround just to help migitate a problem during development. delete it in production build
+      });
   }, []);
 
   return (
