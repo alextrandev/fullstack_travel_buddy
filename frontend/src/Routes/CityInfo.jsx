9aeac2039dcf9b9552c6d883aacf0d9fff7f718c
sheilaonyango <edusheila31@gmail.com>
2024-05-30 12:30:09 +0300

    Frontend:set city data on successful response, log any errors to the console and run the effect whenever the city changes

--- frontend/src/Routes/CityInfo.jsx
+++ frontend/src/Routes/CityInfo.jsx
@@ -5,6 +5,10 @@ const CityInfo = () => {
   useEffect(() => {
     axios
       .get(`${import.meta.env.VITE_API_URL}destinations/${cityName}`)
+      .then((res) => setCityData(res.data)) // Set the city data on successful response
+      .catch((err) => console.log(err)); // Log any errors
+  }, [cityName]); // Run this effect whenever cityName changes
+
   /* Display City name, image and description*/
   return (
     <div className="city-info">
