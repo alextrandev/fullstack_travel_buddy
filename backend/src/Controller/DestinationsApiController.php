7ebf66085119997158fdbca848d1aa6851b958b8
Alex Tran <tranduclong092@gmail.com>
2024-05-30 11:23:04 +0300

    Backend: Feat - create a route for main API controller

--- backend/src/Controller/DestinationsApiController.php
+++ backend/src/Controller/DestinationsApiController.php
@@ -7,13 +7,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/api/destinations/')] // prefix for all api functions
+#[Route('/api/destinations')] // prefix for all api functions
 class DestinationsApiController extends AbstractController
 {
     #[Route('', name: 'app_destinations_api', methods: ['GET'])]
     public function getAll(DestinationRepository $repository): Response
     {
         $destinations = $repository->findAll();
+
+        // return dd($destinations); // for testing
+        return $this->json($destinations);
+    }
+
+    #[Route('/{destination}', name: 'app_single_destination_api', methods: ['GET'])]
+    public function getOne(string $destination, DestinationRepository $repository): Response
+    {
+        $destinations = $repository->findOne($destination);
 
         // return dd($destinations); // for testing
         return $this->json($destinations);
