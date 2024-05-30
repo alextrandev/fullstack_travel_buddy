5cecc5c6d6feeabd0a72f9b0976d31e66f79dcac
Alex Tran <tranduclong092@gmail.com>
2024-05-31 02:24:34 +0300

    Backend: Fix - avoid too many call to backend by move all state hooks to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,11 +7,35 @@ import SearchBar from "./Components/SearchBar.jsx";
 import SearchResult from "./Components/SearchResult.jsx"
 import "./App.css";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 const App = () => {
+  const [destinations, setDestinations] = useState([]);
   const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_API_URL}destinations`)
+      .then((res) => setDestinations(res.data))
+      .catch((err) => {
+        console.log(err)
+        setDestinations([
+          { city: 'Paris', imageUrl: 'https://source.unsplash.com/400x400/?paris' },
+          { city: 'New York', imageUrl: 'https://source.unsplash.com/400x400/?New York' },
+          { city: 'Tokyo', imageUrl: 'https://source.unsplash.com/400x400/?Tokyo' },
+          { city: 'Sydney', imageUrl: 'https://source.unsplash.com/400x400/?Sydney' },
+          { city: 'Cape Town', imageUrl: 'https://source.unsplash.com/400x400/?Cape Town' },
+          { city: 'Rio de Janeiro', imageUrl: 'https://source.unsplash.com/400x400/?Rio de Janeiro' },
+          { city: 'London', imageUrl: 'https://source.unsplash.com/400x400/?London' },
+          { city: 'Dubai', imageUrl: 'https://source.unsplash.com/400x400/?Dubai' },
+          { city: 'Rome', imageUrl: 'https://source.unsplash.com/400x400/?Rome' },
+          { city: 'Barcelona', imageUrl: 'https://source.unsplash.com/400x400/?Barcelona' },
+          { city: 'St. Petersburg', imageUrl: 'https://source.unsplash.com/400x400/?St. Petersburg' },
+          { city: 'Bangkok', imageUrl: 'https://source.unsplash.com/400x400/?Bangkok' },
+        ]) // this workaround just to help migitate a problem during development. delete it in production build
+      });
+  }, []);
 
   const handleSubmit = query => {
     axios
@@ -27,10 +51,9 @@ const App = () => {
           <Navbar />
           <HeroBanner />
           <Routes>
-            <Route path="/" Component={Destination} />
-            <Route path="/city/:cityName" Component={CityInfo} />
+            <Route path="/" element={<Destination destinations={destinations} />} />
+            <Route path="/city/:cityName" element={CityInfo} />
           </Routes>
-          <Destination />
           <SearchBar handleSubmit={handleSubmit} />
           <SearchResult results={results} />
         </div>
--- frontend/src/Components/Destination.jsx
+++ frontend/src/Components/Destination.jsx
@@ -1,33 +1,7 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Destination.css";
 
-const Destination = () => {
-  const [destinations, setDestinations] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get(`${import.meta.env.VITE_API_URL}destinations`)
-      .then((res) => setDestinations(res.data))
-      .catch((err) => {
-        console.log(err)
-        setDestinations([
-          { city: 'Paris', imageUrl: 'https://source.unsplash.com/400x400/?paris' },
-          { city: 'New York', imageUrl: 'https://source.unsplash.com/400x400/?New York' },
-          { city: 'Tokyo', imageUrl: 'https://source.unsplash.com/400x400/?Tokyo' },
-          { city: 'Sydney', imageUrl: 'https://source.unsplash.com/400x400/?Sydney' },
-          { city: 'Cape Town', imageUrl: 'https://source.unsplash.com/400x400/?Cape Town' },
-          { city: 'Rio de Janeiro', imageUrl: 'https://source.unsplash.com/400x400/?Rio de Janeiro' },
-          { city: 'London', imageUrl: 'https://source.unsplash.com/400x400/?London' },
-          { city: 'Dubai', imageUrl: 'https://source.unsplash.com/400x400/?Dubai' },
-          { city: 'Rome', imageUrl: 'https://source.unsplash.com/400x400/?Rome' },
-          { city: 'Barcelona', imageUrl: 'https://source.unsplash.com/400x400/?Barcelona' },
-          { city: 'St. Petersburg', imageUrl: 'https://source.unsplash.com/400x400/?St. Petersburg' },
-          { city: 'Bangkok', imageUrl: 'https://source.unsplash.com/400x400/?Bangkok' },
-        ]) // this workaround just to help migitate a problem during development. delete it in production build
-      });
-  }, []);
+const Destination = ({ destinations }) => {
 
   return (
     <div className="destination">
