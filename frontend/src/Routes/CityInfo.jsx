8d158fe55198b742eba01e9cf18c4534914f5dd3
sheilaonyango <edusheila31@gmail.com>
2024-05-30 12:25:22 +0300

    Frontend: fetch city data from API using city name

--- frontend/src/Routes/CityInfo.jsx
+++ frontend/src/Routes/CityInfo.jsx
@@ -1,6 +1,10 @@
 const CityInfo = () => {
   const { cityName } = useParams();
   const [cityData, setCityData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_API_URL}destinations/${cityName}`)
   /* Display City name, image and description*/
   return (
     <div className="city-info">
