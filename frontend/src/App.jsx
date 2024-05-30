7b20616246c17cacfe73ea6fc47eb5fb79f62cf4
sheilaonyango <edusheila31@gmail.com>
2024-05-30 13:05:01 +0300

    Frontend: Update App.jsx with switch and route components for nested routing and import CityInfo from routes directory

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import HeroBanner from "./Components/HeroBanner.jsx";
 import Destination from "./Components/Destination.jsx";
+import CityInfo from "./Routes/CityInfo";
 
 export default function App() {
   return (
@@ -10,7 +11,10 @@ export default function App() {
         <div className="App">
           <Navbar />
           <HeroBanner />
-          <Destination />
+          <Switch>
+            <Route path="/" Component={Destination} />
+            <Route path="/city/:cityName" Component={CityInfo} />
+          </Switch>
         </div>
       </Router>
     </>
