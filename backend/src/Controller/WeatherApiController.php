b3133843e8dfd4a578983a74d17e3eeffcfb3c36
Alex Tran <tranduclong092@gmail.com>
2024-05-31 23:19:03 +0300

    Backend: Feat - add a check function before send get request to weather api

--- backend/src/Controller/WeatherApiController.php
+++ backend/src/Controller/WeatherApiController.php
@@ -2,23 +2,29 @@
 
 namespace App\Controller;
 
+use App\Repository\DestinationRepository;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class WeatherApiController extends AbstractController
 {
   #[Route('/api/weather/{destination}', name: 'app_weather_api', methods: ['GET'])]
-  public function getWeather(string $destination, LoggerInterface $log, HttpClientInterface $client)
+  public function getWeather(string $destination, DestinationRepository $repository, LoggerInterface $log, HttpClientInterface $client): Response
   {
-    $log->info("Fetching weather info...");
-    $weatherResponse = $client->request(
-      'GET',
-      "https://api.openweathermap.org/data/2.5/weather?q=$destination&appid={$_SERVER['OPENWEATHER_API_KEY']}&units=metric"
-    );
-    $content = $weatherResponse->toArray();
-    $log->info("Weather data sent");
-    return $content;
+    if ($repository->findOne($destination)) {
+      $log->info("Fetching weather info...");
+      $weatherResponse = $client->request(
+        'GET',
+        "https://api.openweathermap.org/data/2.5/weather?q=$destination&appid={$_SERVER['OPENWEATHER_API_KEY']}&units=metric"
+      );
+      $log->info("Weather data sent");
+      $content = $weatherResponse->toArray();
+      return $this->json($content);
+    }
+
+    return $this->json(null);
   }
 }
