1814e50e2ec85f5a4fdd68365a3e0df1a962d027
Alex Tran <tranduclong092@gmail.com>
2024-05-31 19:12:36 +0300

    Backend: Docs - add some comment on codes

--- backend/src/Model/Destination.php
+++ backend/src/Model/Destination.php
@@ -2,11 +2,11 @@
 
 namespace App\Model;
 
+// this is a lightweight class that will give city name and an image url to the front page destination card
 class Destination
 {
   public string $city;
   public string $imageUrl;
-
   public function __construct($city)
   {
     $this->city = $city;
