aef8d567227d2e8d7569f086f84cfe84f40096e1
sheilaonyango <edusheila31@gmail.com>
2024-06-01 17:41:00 +0300

    Frontend: import citydescriptioncss to citydescription.jsx

--- frontend/src/Components/CityDescription.jsx
+++ frontend/src/Components/CityDescription.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import "./CityDescriptionPage.css";
+import "./CityDescription.css";
 
 const CityDescription = () => {
   const { cityName } = useParams();
