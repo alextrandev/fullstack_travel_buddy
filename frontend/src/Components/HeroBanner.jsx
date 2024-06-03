9d8f352f846825e89fd1118603172c89f5ed5439
Alex Tran <tranduclong092@gmail.com>
2024-06-03 19:28:51 +0300

    Frontend: Refactor - re apply the original blur, change h1 color

--- frontend/src/Components/HeroBanner.jsx
+++ frontend/src/Components/HeroBanner.jsx
@@ -1,11 +1,10 @@
 import "./HeroBanner.css";
-import heroImage from "../assets/herobaner1.jpeg";
 
 const HeroBanner = () => {
   return (
-    <div className="hero-banner" style={{ backgroundImage: `url(${heroImage})` }}>
+    <div className="hero-banner">
       <div className="hero-banner__text">
-        <h1>FOREMAP</h1>
+        <h1><span>FORE</span>MAP</h1>
         <p>Traveller little helper</p>
       </div>
     </div>
