720a5044f1584ab05be9e3f42e681313c72c6005
Alex Tran <tranduclong092@gmail.com>
2024-06-03 15:43:43 +0300

    Frontend: Refactor - implement the new component

--- frontend/src/Components/DestinationCard.jsx
+++ frontend/src/Components/DestinationCard.jsx
@@ -1,20 +1,18 @@
-import React from "react";
 import { Link } from "react-router-dom";
-import "./DestinationCard.css";
+import "../Routes/Destination.css";
 
 const DestinationCard = ({ destination }) => {
   return (
-    <div className="destination-card" key={destination.city}>
-      <Link to={`/city/${destination.city}`}>
+    <Link to={`/city/${destination.city}`}>
+      <div className="destination-card">
         <img
           src={destination.imageUrl}
           alt={`Image of ${destination.city}`}
           className="destination-image"
         />
-        <h1>{destination.city}</h1>
-        <p>{destination.description}</p>
-      </Link>
-    </div>
+      </div>
+      <h3 className="destination-name">{destination.city}</h3>
+    </Link>
   );
 };
 
