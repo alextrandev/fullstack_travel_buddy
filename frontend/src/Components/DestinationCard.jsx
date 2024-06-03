29db335f7dff5d2358a9c2dab1c72d47dc9ae92d
sheilaonyango <edusheila31@gmail.com>
2024-06-03 13:30:44 +0300

    Frontend: Define destinationcard component to receive destination prop and display its content

--- frontend/src/Components/DestinationCard.jsx
+++ frontend/src/Components/DestinationCard.jsx
@@ -1,5 +1,17 @@
-const DestinationCard = () => {
-  return <div></div>;
+const DestinationCard = ({ destination }) => {
+  return (
+    <div className="destination-card" key={destination.city}>
+      <Link to={`/city/${destination.city}`}>
+        <img
+          src={destination.imageUrl}
+          alt={`Image of ${destination.city}`}
+          className="destination-image"
+        />
+        <h1>{destination.city}</h1>
+        <p>{destination.description}</p>
+      </Link>
+    </div>
+  );
 };
 
 export default DestinationCard;
