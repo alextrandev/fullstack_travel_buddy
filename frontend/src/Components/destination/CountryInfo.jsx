b9cb02ef101828aa8dc169f585ad013711692745
Alex Tran <tranduclong092@gmail.com>
2024-06-06 11:41:52 +0300

    Frontend: Feat - display the country data

--- frontend/src/Components/destination/CountryInfo.jsx
+++ frontend/src/Components/destination/CountryInfo.jsx
@@ -1,12 +1,15 @@
+import Loading from "../utility/Loading";
 import LoadingImage from "../utility/LoadingImage";
 import "./CountryInfo.css";
 
 export default function CountryInfo({ countryInfo, cityData }) {
+  let currencies, continents, languages, timezones = '';
   if (countryInfo) {
-    console.log(countryInfo);
-  }
-  if (cityData) {
-    console.log(cityData.wikiLink);
+    //turn arrays, objects and nested objects into string
+    currencies = Object.values(countryInfo.currencies).map(currency => Object.values(currency).join(" ")).join(", ");
+    continents = countryInfo.continents.join(", ");
+    languages = Object.values(countryInfo.languages).join(", ");
+    timezones = `${countryInfo.timezones[0]} - ${countryInfo.timezones.slice(-1)}`;
   }
   return (
     <div className="country-info">
@@ -14,14 +17,19 @@ export default function CountryInfo({ countryInfo, cityData }) {
       {!countryInfo
         ? <LoadingImage size={400} text={'Fetching destination infos'} />
         : <ul className="country-info-grid">
-          <li><span></span><b></b></li>
-          <li><span></span><b></b></li>
-          <li><span></span><b></b></li>
-          <li><span></span><b></b></li>
-          <li><span></span><b></b></li>
-          <li><span></span><b></b></li>
-          <li><span></span><b></b></li>
-          <li><a href="">Read more on Wikipedia &gt;</a></li>
+          <li><span>Country: </span><b>{countryInfo.country}</b></li>
+          <li><span>Flag: </span><b>{countryInfo.flag}</b></li>
+          <li><span>Region: </span><b>{countryInfo.subregion}</b></li>
+          <li><span>Continent: </span><b>{continents}</b></li>
+          <li><span>Currency: </span><b>{currencies}</b></li>
+          <li><span>Language: </span><b>{languages}</b></li>
+          <li><span>Timezone: </span><b>{timezones}</b></li>
+          <li>
+            {cityData
+              ? <a href={cityData.wikiLink}>Read more on Wikipedia &gt;</a>
+              : <p>Finding wiki link <Loading type={"dot"} /></p>
+            }
+          </li>
         </ul>
       }
     </div>
