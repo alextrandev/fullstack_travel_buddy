1fd8cdbe5f7f656fbd5a98a6eca172a89e55ad3b
sheilaonyango <edusheila31@gmail.com>
2024-06-06 10:40:01 +0300

    Frontend: Create a useEffect and addEventListener that triggers on scroll

--- frontend/src/Components/BackToTopButton.jsx
+++ frontend/src/Components/BackToTopButton.jsx
@@ -3,6 +3,16 @@ import { useEffect, useState } from "react";
 
 const BackToTopButton = () => {
   const [backToTopButton, setBackToTopButton] = useState(false);
+
+  useEffec(() => {
+    window.addEventListener("Scroll", () => {
+      if (window.scrollY > 100) {
+        setBackToTopButton(true);
+      } else {
+        setBackToTopButton(false);
+      }
+    });
+  });
   return <div></div>;
 };
 
