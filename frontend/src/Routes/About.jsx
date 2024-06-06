50f3fb8dd7c6f313caf268ab2c44bdc6ae125c7a
Alex Tran <tranduclong092@gmail.com>
2024-06-06 16:37:36 +0300

    Front-end: Feat - update about page

--- frontend/src/Routes/About.jsx
+++ frontend/src/Routes/About.jsx
@@ -1,26 +1,51 @@
 import './About.css';
-import Divider from "../Components/layout/Divider.jsx";
 
 const About = () => {
   return (
     <>
       <div className="about-section">
         <h1 id="about"> About Us</h1>
-        <h3>
-          Hello, we are ASA team! We are a group of passionate and
-          enthusiastic junior developers who are excited about building amazing
-          web applications and learning new technologies. Our team is dedicated to
-          growing our skills and contributing to meaningful projects. We believe
-          in collaboration, continuous learning, and pushing the boundaries of
-          what we can achieve.Our Foremap App is the first project of us as a junior web developer, designed to showcase our skills, which we gained during our first semesters in Helsinki Business College. This project aims to create an online presence that highlights our abilities in HTML, CSS,  JavaScript, React and Symphony, while also incorporating responsive design principles to ensure the site looks great on all devices.
-          By completing this project, we have had to:gain hands-on experience in building a complete website from scratch, as well as we understand the fundamentals of web design and development.
-          We tried to prectice how to create a responsive design that works on various devices with our app.
-          Our skills in using in using HTML, CSS, React and Symfony have growned up signficaly.
-          Also one really important practice for us using version control with Git and GitHub.
+        <div>
+          <div>
+            <b>
+              Hello world! We are ASA team!
+            </b>
+            <p>
+              A group of passionate and enthusiastic junior developers who are excited about building amazing web applications and learning new technologies.
+            </p>
+            <p>
+              Our team is dedicated to growing our skills and contributing to meaningful projects.
+            </p>
+            <p>
+              We believe in collaboration, continuous learning, and pushing the boundaries of what we can achieve.
+            </p>
+          </div>
+        </div>
+        <div>
 
-
-        </h3>
-        <Divider />
+          <b>Foremap - A travel buddy project</b>
+          <p>
+            Our first project as a junior web developer, designed to showcase our skills, which we gained during our first semesters in Helsinki Business College.
+          </p>
+          <p>
+            This project aims to create an online presence that highlights our abilities in HTML, CSS,  JavaScript, React and Symphony, while also incorporating responsive design principles to ensure the site looks great on all devices.
+          </p>
+        </div>
+        <div>
+          <b>What we learn from this collaborating project</b>
+          <p>
+            By completing this project, we gains hands-on experience in building a complete website from scratch.
+          </p>
+          <p>
+            We also get to familiarize ourselves with how a fullstack website works.
+          </p>
+          <p>
+            Our skills in using in using React and Symfony and various libraries and pakages have growned up signficaly. Also in using version control with Git and GitHub.
+          </p>
+          <p>
+            We got to practice good teamwork practices like writing clean code, provide clean commit message, project management, create a responsive design, etc.
+          </p>
+        </div>
       </div>
     </>
   );
