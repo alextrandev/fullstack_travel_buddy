d42dfafde1a8e0b32d612e548bb502c0f22cd04d
sheilaonyango <edusheila31@gmail.com>
2024-06-06 11:16:09 +0300

    Frontend: Add css for styling and import it to BackToTopButton.jsx

--- frontend/src/Components/BackToTopButton.jsx
+++ frontend/src/Components/BackToTopButton.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import "./BackToTopButton.css";
 
 const BackToTopButton = () => {
   const [backToTopButton, setBackToTopButton] = useState(false);
